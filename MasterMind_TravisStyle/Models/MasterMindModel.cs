dc0164c1909422051549caeef1479963384e8374
tbay007 <tb007magetech@gmail.com>
2024-06-27 11:04:33 -0400

    Pushing first changes to Dev Branch

---
+++ MasterMind_TravisStyle/Models/MasterMindModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind_TravisStyle.Models
+{
+    public class MasterMindModel
+    {
+        public MasterMindModel() 
+        { 
+        
+        }
+        public long Number1 { get; set; }
+        public long Number2 { get; set; }
+        public long Number3 { get; set; }
+        public long Number4 { get; set; }
+    }
+}
